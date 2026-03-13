bba92db825a9553e37e8e01de47e50e5c1f8cd03
Esteban Montoya <estebanmontoya@Estebans-MacBook-Air.local>
2026-03-13 16:38:42 -0600

    Some java docs

--- code/app/src/main/java/com/example/eventlotterysystemapp/data/models/UserSession.java
+++ code/app/src/main/java/com/example/eventlotterysystemapp/data/models/UserSession.java
@@ -1,5 +1,7 @@
 package com.example.eventlotterysystemapp.data.models;
-
+/**
+ * User Session class to know which user is currently logged in
+ */
 public class UserSession {
 
     private static User currentUser;
